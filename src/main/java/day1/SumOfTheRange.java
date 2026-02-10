d0a05fc8239dabe54163a9bad217ac4b161ae6a5
LAPTOP-NQ67GLI5\김다은 <tmzl235@naver.com>
2026-02-10 10:48:01 +0900

    Code Kata 22

---
+++ src/main/java/day1/SumOfTheRange.java
@@ -0,0 +1,40 @@
+package day1;
+
+public class SumOfTheRange {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        long result = sol.solution(3, 5);
+        System.out.println(result);
+
+        result = sol.solution(3, 3);
+        System.out.println(result);
+
+        result = sol.solution(5, 3);
+        System.out.println(result);
+    }
+
+    private static class Solution {
+        public long solution(int a, int b) {
+            long answer = 0;
+            if (a == b) return a;
+
+            if (Math.max(a, b) == a) {
+                answer = range(a, b);
+            } else {
+                answer = range(b, a);
+            }
+
+            return answer;
+        }
+
+        private long range(int a, int b) {
+            long answer = 0;
+            for (int i = b; i <= a; i++) {
+                answer += i;
+            }
+
+            return answer;
+        }
+    }
+}
