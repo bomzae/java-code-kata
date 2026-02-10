508c8153e9202f2c62960a865e66864b1cf1e862
LAPTOP-NQ67GLI5\김다은 <tmzl235@naver.com>
2026-02-10 10:36:46 +0900

    init commit

---
+++ src/main/java/day1/HarshadNumber.java
@@ -0,0 +1,37 @@
+package day1;
+
+public class HarshadNumber {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        boolean result = sol.solution(10);
+        System.out.println(result);
+
+        result = sol.solution(11);
+        System.out.println(result);
+
+        result = sol.solution(12);
+        System.out.println(result);
+
+        result = sol.solution(13);
+        System.out.println(result);
+    }
+
+    private static class Solution {
+        public boolean solution(int x) {
+            boolean answer = true;
+            int num = sum(x);
+            if (x % num != 0) answer = false;
+
+            return answer;
+        }
+
+        private int sum(int num) {
+            if (num < 10) {
+                return num;
+            }
+
+            return (num % 10) + sum(num / 10);
+        }
+    }
+}
